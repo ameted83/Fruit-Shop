e37751282b743d741df4a6af184fbf508994ff8a
Amelia Tedeschi <tedeschiamelia@gmail.com>
2022-05-25 18:40:58 +0200

    lading-page-v.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <LandingPage />
     </div>
   );
 }
